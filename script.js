4a8028f8bc472410111a90b8ee3fb9bce622c138
rat.7 <104742078+rat7dev@users.noreply.github.com>
2022-09-08 17:43:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+$("img").attr("draggable","false")
+$(".h3").css("display", "none")
+$(".m-main").css("display", "none")
+$(".c-main").css("display", "none")
+$(".br").css("display", "none")
+
+function btn1() {
+    $(".m-main").css("display", "flex")
+    $(".h2").css("display", "none")
+    $(".h3").css("display", "flex")
+}
+function btn2() {
+    $(".m-main").css("display", "none")
+    $(".h2").css("display", "flex")
+    $(".h3").css("display", "none")
+}
+function btn3() {
+    $(".c-main").css("display", "flex")
+    $(".m-main").css("display", "none")
+    $(".h2").css("display", "flex")
+    $(".h3").css("display", "none")
+}
+function btn4() {
+    $(".c-main").css("display", "none")
+    $(".br").css("display", "flex")
+    $(".eng").css("display", "none")
+}
+function btn5() {
+    $(".c-main").css("display", "none")
+    $(".br").css("display", "none")
+    $(".eng").css("display", "flex")
+}
+function btnc() {
+    $(".c-main").css("display", "none")
+    $(".m-main").css("display", "none")
+    $(".h2").css("display", "flex")
+    $(".h3").css("display", "none")
+}
+
+console.log("Made with ❤️ by Rat Dev | Todos os direitos reservados ©")
